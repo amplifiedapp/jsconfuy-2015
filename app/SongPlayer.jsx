68ada17d908cfbebb6f16db9a117a373f4ea698d
Sergio Rafael Gianazza <sgianazza@gmail.com>
2015-03-25 18:24:24 -0300

    simplify player status

--- app/SongPlayer.jsx
+++ app/SongPlayer.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import SongWaveform from './SongWaveform'
 import SongWaveformComments from './SongWaveformComments'
 
+const PLAYING = '1';
+const PAUSED = '2';
+const STOPPED = '3';
+
 export default class SongPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerStatus: STOPPED
+    };
+  }
+
   componentWillMount() {
     this.sound = soundManager.createSound({
       url: this.props.song.get('audio')
@@ -11,34 +22,49 @@ export default class SongPlayer extends React.Component {
 
   handlePlayClick() {
     this.sound.play();
+    this.setState({
+      playerStatus: PLAYING
+    });
   }
 
   handlePauseClick() {
     this.sound.pause();
+    this.setState({
+      playerStatus: PAUSED
+    });
   }
 
   handleResumeClick() {
     this.sound.resume();
+    this.setState({
+      playerStatus: PLAYING
+    });
   }
 
   handleStopClick() {
     this.sound.stop();
+    this.setState({
+      playerStatus: STOPPED
+    });
   }
 
   render() {
+    let playerControls = [];
+    if (this.state.playerStatus === STOPPED) {
+      playerControls.push(<a href='#' onClick={this.handlePlayClick.bind(this)}>Play</a>);
+    }
+
+    if (this.state.playerStatus === PLAYING) {
+      playerControls.push(<a href='#' onClick={this.handlePauseClick.bind(this)}>Pause</a>);
+      playerControls.push(<a href='#' onClick={this.handleStopClick.bind(this)}>Stop</a>);
+    }
+
+    if (this.state.playerStatus === PAUSED) {
+      playerControls.push(<a href='#' onClick={this.handleResumeClick.bind(this)}>Resume</a>)
+    }
+
     return <div>
-      <a href="#" onClick={this.handlePlayClick.bind(this)}>
-        Play
-      </a>
-      <a href="#" onClick={this.handlePauseClick.bind(this)}>
-        Pause
-      </a>
-      <a href="#" onClick={this.handleResumeClick.bind(this)}>
-        Resume
-      </a>
-      <a href="#" onClick={this.handleStopClick.bind(this)}>
-        Stop
-      </a>
+      {playerControls}
       <div className="waveform-container">
         <SongWaveform waveformData={this.props.song.get('waveformData')}/>
         <SongWaveformComments />
