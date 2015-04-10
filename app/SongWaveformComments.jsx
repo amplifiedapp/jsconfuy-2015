91a545da557ce7376194f54daf8ccb9f1849f05f
Leonardo Andres Garcia Crespo <leoasis@gmail.com>
2015-04-10 20:38:56 -0300

    Improve comment markers styles and components

--- app/SongWaveformComments.jsx
+++ app/SongWaveformComments.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import moment from "moment";
-import classnames from "classnames";
 import NewComment from "./NewComment";
 import intents from "./intents";
 import connectStreamsToInput from "./connectStreamsToInput";
@@ -8,69 +7,78 @@ import connectStreamsToInput from "./connectStreamsToInput";
 class SongWaveformComments extends React.Component {
 
   renderCommentMarker(comment) {
-    let handleClass;
-    let handleImageUrl;
-    let markerStyle;
+    const imageUrl = comment.get('user').get('avatar_url');
+    const isCurrentComment = this.props.currentCommentCid === comment.get('cid');
 
-    handleImageUrl = comment.get('user').get('avatar_url');
-    handleClass = classnames({
-      'waveform__marker__handle': true,
-      'handle-with-image': !!handleImageUrl
-    });
-
-    markerStyle = {
-      left: comment.get('song_moment_percentage') + '%',
-      height: 140
-    };
-
-    return <div className="waveform__marker" style={markerStyle} key={comment.get('cid')}>
-              <div className="waveform__marker__positioner"></div>
-              <div className={handleClass} onMouseEnter={() => { this.props.commentEnter(comment.get('cid')) }} onMouseLeave={this.props.commentLeave}>
-                <img src={handleImageUrl} />
-              </div>
-          </div>;
+    return <Marker position={comment.get('song_moment_percentage')}
+      onShow={() => { this.props.commentEnter(comment.get('cid')) }}
+      onHide={this.props.commentLeave}
+      handleImageUrl={imageUrl}
+      handleWidth={isCurrentComment ? 30 : 20} />;
   }
 
-  renderCurrentComment() {
-    let comment, user, userAvatar;
-    comment = this.props.comments.get(this.props.currentCommentCid);
-    user = comment.get("user");
-    if (user.get("avatar_url")) {
-      userAvatar = <img src={user.get("avatar_url")} />;
-    } else {
-      userAvatar = <noscript />;
-    }
-    return <div>
-            <div className="waveform-comment__box">
-              {userAvatar}
-              <div>{user.get("full_name")}</div>
-              <span className="duration">
-                  {moment.utc(comment.get("song_moment") * 1000).format("H:mm:ss").replace(/^0:/, "")}
-              </span>
-              <span>
-                  {comment.get("comment")}
-              </span>
-            </div>
-          </div>;
+  renderCommentBox() {
+    if (!this.props.currentCommentCid) return null;
+
+    const comment = this.props.comments.get(this.props.currentCommentCid);
+    const user = comment.get("user");
+
+    const percentage = comment.get('song_moment_percentage');
+    const commentStyles = percentage < 50 ?
+      {
+        left: percentage + '%',
+        marginLeft: -15
+      } :
+      {
+        right: (100 - percentage) + '%',
+        marginRight: -15
+      };
+
+    return <div className="waveform-comment__box" style={commentStyles}>
+      <span className="commentUsername">{user.get("full_name")}</span>
+      {' '}
+      <span className="commentSongDuration">
+          ({moment.utc(comment.get("song_moment") * 1000).format("H:mm:ss").replace(/^0:/, "")})
+      </span>
+      {' '}
+      <span className="commentText">
+          {comment.get("comment")}
+      </span>
+    </div>;
   }
 
   render() {
-    let markers = this.props.comments.map(this.renderCommentMarker.bind(this)).toArray();
-    let currentComment;
-
-    if(this.props.currentCommentCid !== null) {
-      currentComment = this.renderCurrentComment();
-    } else {
-      currentComment = <noscript />;
-    }
+    const markers = this.props.comments.map(c => this.renderCommentMarker(c)).toArray();
 
     return <div>
             <div className="waveform__layer">
-              <div>{markers}</div>
+              {markers}
             </div>
-            {currentComment}
+            {this.renderCommentBox()}
             <NewComment addingComment={this.props.addingComment}/>
           </div>;
+  }
+}
+
+class Marker extends React.Component {
+  render() {
+    const markerStyle = {
+      left: this.props.position + '%',
+      height: 140
+    };
+
+    const handleStyle = {marginLeft: -(this.props.handleWidth / 2)}
+
+    const markerHandle = <img src={this.props.handleImageUrl} style={handleStyle} />
+
+    return <div className="waveform__marker" style={markerStyle}
+      onMouseEnter={() => this.props.onShow()}
+      onMouseLeave={() => this.props.onHide()}>
+        <div className="waveform__marker__positioner"></div>
+        <div className="waveform__marker__handle" style={{width: this.props.handleWidth}}>
+          {markerHandle}
+        </div>
+    </div>;
   }
 }
 
