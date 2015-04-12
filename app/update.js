25837ba7a30b8787b9c47a36d54009f103069191
Leonardo Andres Garcia Crespo <leoasis@gmail.com>
2015-04-11 23:11:26 -0300

    Avoid extra song property in state

--- app/update.js
+++ app/update.js
@@ -5,60 +5,60 @@ export default function update(state, action) {
   console.log(action);
   switch (action.type) {
     case 'PLAY_SONG':
-      return state.setIn(['currentRehearsedSong', 'song', 'playStatus'],
+      return state.setIn(['currentRehearsedSong', 'playStatus'],
         'PLAYING'
       );
     case 'STOP_SONG':
-      return state.updateIn(['currentRehearsedSong', 'song'], (song) => {
+      return state.updateIn(['currentRehearsedSong'], (song) => {
         return song.set('playStatus', 'STOPPED').set('playedPercentage', 0);
       });
     case 'PAUSE_SONG':
-      return state.setIn(['currentRehearsedSong', 'song', 'playStatus'],
+      return state.setIn(['currentRehearsedSong', 'playStatus'],
         'PAUSED'
       );
     case 'RESUME_SONG':
-      return state.setIn(['currentRehearsedSong', 'song', 'playStatus'],
+      return state.setIn(['currentRehearsedSong', 'playStatus'],
         'PLAYING'
       );
     case 'LOADING_SONG':
-      return state.setIn(['currentRehearsedSong', 'song', 'loadedPercentage'],
+      return state.setIn(['currentRehearsedSong', 'loadedPercentage'],
         action.payload.loadedPercentage
       );
     case 'PLAYING_SONG':
-      const durationInSecs = state.getIn(['currentRehearsedSong', 'song', 'durationInSecs']);
-      return state.setIn(['currentRehearsedSong', 'song', 'playedPercentage'],
+      const durationInSecs = state.getIn(['currentRehearsedSong', 'durationInSecs']);
+      return state.setIn(['currentRehearsedSong', 'playedPercentage'],
         100 * action.payload.positionInSecs / durationInSecs
       );
     case 'FINISH_PLAYING_SONG':
-      return state.updateIn(['currentRehearsedSong', 'song'], (song) => {
+      return state.updateIn(['currentRehearsedSong'], (song) => {
         return song.set('playStatus', 'STOPPED').set('playedPercentage', 0);
       });
     case 'SEEK_IN_SONG':
-      return state.updateIn(['currentRehearsedSong', 'song'], (song) => {
+      return state.updateIn(['currentRehearsedSong'], (song) => {
         return song.set('playedPercentage', action.payload.percentage);
       });
     case 'VIEW_COMMENT':
-      return state.setIn(['currentRehearsedSong', 'song', 'currentCommentCid'],
+      return state.setIn(['currentRehearsedSong', 'currentCommentCid'],
         action.payload.commentCid
       );
     case 'NEW_COMMENT':
-      return state.setIn(['currentRehearsedSong', 'song', 'addingComment'],
+      return state.setIn(['currentRehearsedSong', 'addingComment'],
         true
       );
     case 'HIDE_NEW_COMMENT':
-      return state.setIn(["currentRehearsedSong", "song", "addingComment"],
+      return state.setIn(["currentRehearsedSong", "addingComment"],
         false
       );
     case 'CREATE_COMMENT':
-      return state.setIn(["currentRehearsedSong", "song", "addingComment"],
+      return state.setIn(["currentRehearsedSong", "addingComment"],
         false
       );
     case 'CREATE_COMMENT_SUCCESS':
-      const song = state.getIn(['currentRehearsedSong', 'song']);
+      const song = state.getIn(['currentRehearsedSong']);
       const lastCid = song.getIn(['comments'])
         .keySeq().max();
       const newCid = lastCid + 1;
-      return state.updateIn(["currentRehearsedSong", "song", "comments"], (comments) => {
+      return state.updateIn(["currentRehearsedSong", "comments"], (comments) => {
         const comment = assign({}, action.payload.comment, {cid: newCid});
         comment.songMoment = comment.songMomentPercentage * song.get('durationInSecs') / 100;
         return comments.set(newCid, Immutable.fromJS(comment));
